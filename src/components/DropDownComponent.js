54f1a8b7c2fa63fda1999c2822baaaa0a9153ee8
oal2727 <joseluis_1555@hotmail.com>
2021-01-22 16:51:42 -0500

    fix component

--- src/components/DropDownComponent.js
+++ src/components/DropDownComponent.js
@@ -4,9 +4,7 @@ import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Grow from '@material-ui/core/Grow';
 import Paper from '@material-ui/core/Paper';
 import Popper from '@material-ui/core/Popper';
-import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
-import {Link} from "react-router-dom"
 
 
 
